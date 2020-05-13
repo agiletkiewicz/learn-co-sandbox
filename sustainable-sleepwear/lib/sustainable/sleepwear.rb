6caec1fefa7ac9f0954ccf8b67bfe9a5e7836136
Agi Letkiewicz <a.letkiewicz@gmail.com>
2020-05-13 21:42:11 +0000

    first commit

---
+++ sustainable-sleepwear/lib/sustainable/sleepwear.rb
@@ -0,0 +1,8 @@
+require "sustainable/sleepwear/version"
+
+module Sustainable
+  module Sleepwear
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ sustainable-sleepwear/lib/sustainable/sleepwear/version.rb
@@ -0,0 +1,5 @@
+module Sustainable
+  module Sleepwear
+    VERSION = "0.1.0"
+  end
+end
